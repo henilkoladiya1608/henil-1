0a1fd92dc669cc01f4554eed5c37679183629db4
Henil  Koladiya <koladiyahenil08@gmail.com>
2025-07-07 18:09:53 +0530

    Add files via upload

--- fact.c
+++ fact.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int factorial(int n);
-int main(){
-int num;
+int main()
+{
+    int num;
 
-printf("Enter the number :");
-scanf("%d", &num);
-int result = factorial(num);
-printf("The factorial value is :%d",result);
+    printf("Enter the number :");
+    scanf("%d", &num);
+    int result = factorial(num);
+    printf("The factorial value is :%d", result);
 
     return 0;
 }
-int factorial(int n){
-       int fact = 1;
+int factorial(int n)
+{
+    int fact = 1;
     for (int i = 1; i <= n; i++)
     {
- 
-    fact = fact * i ;
+
+        fact = fact * i;
     }
     return fact;
 }
-
-
