b03771cfbd7efb80b47d4fafd7e87d27911c5efe
Henil  Koladiya <koladiyahenil08@gmail.com>
2025-07-19 21:29:08 +0530

    Add files via upload

---
+++ intializestructer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+struct  student
+{
+  int roll;
+  float cgpa;
+  char name[100];
+
+};
+int main(){
+     
+    struct student s1 = {1664, 9.2535 , "Henil"};
+    
+    printf("Student cgpa is :%f \n", s1.cgpa);
+    strcpy(s1.name , "henil");
+    printf("Student name is :%s \n", s1.name);
+    printf("Student roll number is :%d \n", s1.roll);
+
+    return 0;
+}
