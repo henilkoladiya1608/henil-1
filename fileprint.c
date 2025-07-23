2cb881895c0bd9424a44d6da100f54b8667da7b9
Henil  Koladiya <koladiyahenil08@gmail.com>
2025-07-23 15:21:40 +0530

    Add files via upload

---
+++ fileprint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    FILE *fptr;
+    fptr = fopen("Newtext.txt", "w");
+
+    fprintf(fptr, "%c" , 'm');
+    fprintf(fptr, "%c" , 'a');
+    fprintf(fptr, "%c" , 'n');
+    fprintf(fptr, "%c" , 'g');
+    fprintf(fptr, "%c" , 'o');
+    fclose(fptr);
+
+    return 0;
+}
