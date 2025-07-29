d8ad3e9fdbf891d299fa0e725635e149831860ca
Henil  Koladiya <koladiyahenil08@gmail.com>
2025-07-29 15:28:42 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int cube;
+    cout << "Enter number " <<endl;
+    cin >> n;
+    cube = n*n*n;
+
+    cout<< cube << endl;
+ 
+}
